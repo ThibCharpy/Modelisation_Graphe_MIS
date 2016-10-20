5fa3541f74183a017d19ef50721c5f32b74e3e07
thibault <thibault.charpignon@wanadoo.fr>
2016-10-20 18:25:01 +0200

    pull branch master

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
  * Created by thibault on 17/10/16.
  */
 public class Main {
+
     public static void main(String[] args) {
         try {
             Graphe g = new Graphe("src/main/ressources/g1.graphe");
@@ -23,4 +24,5 @@ public class Main {
             e.printStackTrace();
         }
     }
+
 }
