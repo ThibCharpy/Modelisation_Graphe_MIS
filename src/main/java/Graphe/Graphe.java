ad89270b1a7c86306d4fc8c25e86d726773fb380
thibault <thibault.charpignon@wanadoo.fr>
2016-10-20 15:30:08 +0200

    pull branch master envThomas changes

--- src/main/java/Graphe/Graphe.java
+++ src/main/java/Graphe/Graphe.java
@@ -28,19 +28,19 @@ public class Graphe {
         vertexesSet = new HashSet<Vertex>();
     }
 
-    public Graphe(String path) throws FilePathExcpetion, FileNotFoundException {
+    public Graphe(String path) throws FilePathException, FileNotFoundException {
         int cpt = 0;
         if ('.' == path.charAt(0))
-            throw new FilePathExcpetion("Empty File name Error.");
+            throw new FilePathException("Empty File name Error.");
         while(cpt < path.length() && '.' != path.charAt(cpt)){
             if (' ' != path.charAt(cpt) || '/' != path.charAt(cpt)  || '\\' != path.charAt(cpt)
                     || '!' != path.charAt(cpt) || '?' != path.charAt(cpt))
                 cpt++;
             else
-                throw new FilePathExcpetion("File path syntax Error.");
+                throw new FilePathException("File path syntax Error.");
         }
         if('.' != path.charAt(cpt))
-            throw new FilePathExcpetion("File path extension Error.");
+            throw new FilePathException("File path extension Error.");
         else
             cpt++;
 
@@ -51,7 +51,7 @@ public class Graphe {
         }
 
         if (!builder.toString().equals("graphe"))
-            throw new FilePathExcpetion("File path extension Error.");
+            throw new FilePathException("File path extension Error.");
 
         Path source = Paths.get(path);
 
