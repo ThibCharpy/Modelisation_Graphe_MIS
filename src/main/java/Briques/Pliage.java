e6e2cb24a2590b94b15feecc653e331dcb7a1649
benoitgallet <benoit.gallet@live.fr>
2016-10-24 13:47:56 +0200

    Ajout d'une fonction ou deux dans Vertex

---
+++ src/main/java/Briques/Pliage.java
@@ -0,0 +1,13 @@
+package Briques;
+
+import Graphe.*;
+
+/**
+ * Created by benoit-windows on 23/10/2016.
+ */
+public class Pliage {
+
+    public static void pliage(Graphe g, Vertex v){
+
+    }
+}
